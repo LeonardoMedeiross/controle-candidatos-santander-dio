50942d42ef63bab5bfb701b793709c56568f141e
Leonardo Medeiros <leo-sbs@live.com>
2024-06-07 00:03:05 -0300

    fazendo ligações para os candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,39 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-imprimirSelecionados();
+        String [] candidatos = {"Leonardo","Gustavo" ,"Crystiane", "Jonas","Miguel"};
+        for (String candidato : candidatos){
+            entrandoEmContato(candidato);
+
+        }
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizadas ++ ;
+            }else {
+                System.out.println("Contato realizado com sucesso ");
+            }
+        }while (continuarTentando && tentativasRealizadas <3);
+
+        if (atendeu)
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        else
+            System.out.println("Não conseguimos contato com " + candidato + " número maximo de tentativas : " + tentativasRealizadas + " realizadas");
+    }
+    //metodo auxiliar
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
     static void imprimirSelecionados (){
         String [] candidatos = {"Leonardo","Gustavo" ,"Crystiane", "Jonas","Miguel"};
